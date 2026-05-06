18e6d0aa470188116ed6c358c31adbc048bd142a
Margaret Wambui <margaretwambui294@gmail.com>
2026-05-06 22:10:31 +0300

    refactor: Remove unused authentication and web controllers, and update User model structure

--- tukac-portal/src/main/java/com/tukac/App.java
+++ tukac-portal/src/main/java/com/tukac/App.java
@@ -1,12 +1,30 @@
 package com.tukac;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.tukac.db.Database;
+import com.tukac.ui.panels.MainFrame;
 
-@SpringBootApplication
+import javax.swing.*;
+
 public class App {
-
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        try {
+            FlatLightLaf.setup();
+            UIManager.put("Button.arc", 8);
+            UIManager.put("Component.arc", 8);
+            UIManager.put("TextComponent.arc", 8);
+            UIManager.put("ScrollBar.width", 10);
+            UIManager.put("ScrollBar.thumbArc", 999);
+            UIManager.put("ScrollBar.trackArc", 999);
+        } catch (Exception e) {
+            System.err.println("Failed to set look and feel");
+        }
+
+        Database.initialize();
+
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
     }
 }
