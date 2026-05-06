4c351c1a716e80fd93a87697799ec261477061b6
Margaret Wambui <margaretwambui294@gmail.com>
2026-05-06 11:48:39 +0300

    feat: Implement main application entry point with MainFrame initialization

---
+++ tukac-portal/src/main/java/com/tukac/App.java
@@ -0,0 +1,17 @@
+package com.tukac;
+
+import com.tukac.ui.panels.MainFrame;
+import javax.swing.SwingUtilities;
+
+public class App {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame frame = new MainFrame();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
