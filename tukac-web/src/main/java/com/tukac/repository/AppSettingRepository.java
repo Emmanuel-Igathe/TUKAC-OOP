42189c9e4b1a44c9cf4e310594a64f1064fbb813
Margaret Wambui <margaretwambui294@gmail.com>
2026-05-13 18:01:03 +0300

    Remove logo SVG and profile HTML files from the project

--- tukac-web/src/main/java/com/tukac/repository/AppSettingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tukac.repository;
-
-import com.tukac.model.AppSetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppSettingRepository extends JpaRepository<AppSetting, String> {}
